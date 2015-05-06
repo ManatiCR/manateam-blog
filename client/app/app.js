9ec0a736fd789c1705f087389ca9685c04ae2a04
Rodrigo Espinoza <espinoza.brenes.rodrigo@gmail.com>
2015-05-05 23:09:36 -0600

    Se inicia con la configuración multi-idioma

--- client/app/app.js
+++ client/app/app.js
@@ -4,16 +4,30 @@ angular.module('manatiBlogApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ui.router'
+  'ui.router',
+  'pascalprecht.translate',
 ])
 .constant('appConfig',{
   api: {
     url: 'http://manati.tk/api/'
   }
 })
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider
-      .otherwise('/');
-
-    $locationProvider.html5Mode(false);
+.config(function ($stateProvider, $urlRouterProvider, $locationProvider,$translateProvider) {
+  $urlRouterProvider
+  .otherwise('/');
+  $locationProvider.html5Mode(false);
+  $translateProvider.translations('en',{
+    'MANATI-SLOGAN': 'We’re a bunch of Drupal geeks'
   });
+  $translateProvider.translations('es',{
+    'MANATI-SLOGAN': 'Somos un montón de Drupal geeks'
+  });
+  $translateProvider.registerAvailableLanguageKeys(['en', 'es'], {
+  'en_US': 'en',
+  'en_UK': 'en',
+  'es_ES': 'es',
+  'es_CL': 'es'
+});
+  $translateProvider.determinePreferredLanguage();
+  $translateProvider.useCookieStorage();
+});
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,8 +1,15 @@
 'use strict';
 
 angular.module('manatiBlogApp')
-  .controller('MainCtrl', function ($scope,$stateParams,dataFactory) {
+  .controller('MainCtrl', function ($scope,$stateParams,dataFactory,$translate) {
       $scope.posts = [];
+      $translate.use();
+      $scope.setLanguage = function(lang){
+        console.log(lang);
+        $translate.use(lang);
+      };
+
+
       console.log($stateParams.page);
       dataFactory.getPosts($stateParams.page).then(function(data){
         $scope.posts = data;

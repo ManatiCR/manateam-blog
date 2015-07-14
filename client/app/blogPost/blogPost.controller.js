2fd6e55bf38cf869265b562536db73afbbe1ec88
Kevin Porras <kporras07@gmail.com>
2015-07-14 13:32:35 -0600

    Removed unused code.

--- client/app/blogPost/blogPost.controller.js
+++ client/app/blogPost/blogPost.controller.js
@@ -3,24 +3,18 @@
 angular.module('manatiBlogApp')
   .controller('BlogPostCtrl', function ($scope,$rootScope,$stateParams,dataFactory, $translate, $state) {
     $scope.post = [];
-    $scope.langReverted = false;
     function getPostByid(id, lang){
       dataFactory.getPostById(id,lang).then(function(data){
         if (data[0] != undefined) {
           $scope.post = data[0];
-          $scope.langReverted = false;
         }
-        else if (!$scope.langReverted) {
-          $scope.langReverted = true;
+        else {
           var newLang = lang == 'es' ? 'en' : 'es';
           $scope.flashMessage = "The requested post doesn't exist in requested language. Falling back to actual language.";
           $translate.use(newLang);
         }
-        else {
-        }
      }).catch (function(e) {
          if (e.status == 404) {
-           $scope.langReverted = false;
            $state.go('not-found');
          }
      });

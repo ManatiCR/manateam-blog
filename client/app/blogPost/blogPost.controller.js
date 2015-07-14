14e0ef10e7bfcffdcedb496890c6d480bf887d76
Kevin Porras <kporras07@gmail.com>
2015-07-14 13:04:53 -0600

    Handling non-existing post for a given language and 404 on blogPost.

--- client/app/blogPost/blogPost.controller.js
+++ client/app/blogPost/blogPost.controller.js
@@ -1,11 +1,27 @@
 'use strict';
 
 angular.module('manatiBlogApp')
-  .controller('BlogPostCtrl', function ($scope,$rootScope,$stateParams,dataFactory, $translate) {
+  .controller('BlogPostCtrl', function ($scope,$rootScope,$stateParams,dataFactory, $translate, $state) {
     $scope.post = [];
+    $scope.langReverted = false;
     function getPostByid(id, lang){
       dataFactory.getPostById(id,lang).then(function(data){
-        $scope.post = data[0];
+        if (data[0] != undefined) {
+          $scope.post = data[0];
+          $scope.langReverted = false;
+        }
+        else if (!$scope.langReverted) {
+          $scope.langReverted = true;
+          var newLang = lang == 'es' ? 'en' : 'es';
+          $translate.use(newLang);
+        }
+        else {
+        }
+     }).catch (function(e) {
+         if (e.status == 404) {
+           $scope.langReverted = false;
+           $state.go('not-found');
+         }
      });
     }
     $rootScope.$on('$translateChangeSuccess', function(){
